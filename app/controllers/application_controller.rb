2eedb2a9dd8cd36cee990376edaf8526eb77fc4f
Mauricio Miranda <maurimiranda@gmail.com>
2012-11-14 12:37:29 -0300

    cambio simple en el texto del banner en inglés

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :set_locale
- 
+
   def set_locale
     I18n.locale = get_from_host || I18n.default_locale
     @other_locale = I18n.locale == :es ? :en : :es
