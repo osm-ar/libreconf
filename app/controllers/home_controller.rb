f98435c351f6e54ce35c4db2150747848dbc37e9
Mauricio Miranda <maurimiranda@gmail.com>
2013-03-08 16:55:20 -0300

    abstracts ordered by creation date

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
     @speakers = Speaker.all
     @sponsors = Sponsor.all(:order => :sponsor_type_id)
     @workshops = Workshop.all
-    @abstracts = AbstractStatus.find(AbstractStatus::APPROVED).abstracts.order('is_key desc')
+    @abstracts = AbstractStatus.find(AbstractStatus::APPROVED).abstracts.order('is_key desc, created_at')
   end
 end
